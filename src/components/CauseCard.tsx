aee19d30e35ec83d60396fbef55cf84c210f5616
Rokeeb Qasim <qasimrokeeb@gmail.com>
2025-10-23 13:48:53 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/CauseCard.tsx
+++ src/components/CauseCard.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
+import React from 'react';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Progress } from '@/components/ui/progress';
